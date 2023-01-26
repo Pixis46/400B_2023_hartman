75f651d341d14b324a4d6337a37f7bcbc62c5b26
Peter Hartman <phartmandebate@gmail.com>
2023-01-26 15:16:11 -0700

    Added Lab1 updated and .gitignore change

--- Labs/Lab1/Lab1_VLSR.py
+++ Labs/Lab1/Lab1_VLSR.py
@@ -32,9 +32,31 @@ import numpy as np # import numpy
 import astropy.units as u # import astropy units
 from astropy import constants as const # import astropy constants
 
+def VLSR(R_0, mu=6.379, vsun=12.24*u.km/u.s):
+    '''
+    Compute VLSR from a given stellar radius with
+        VLSR = 4.74*mu*R_0 - vsun
+    Parameters:
+        R_0: Solar Radius in kpc (Astropy Quantity)
+        mu: Proper Motion of Sag A* in mas/yr (Astropy Quantity),
+            default from Reid & Brunthaler 2004
+        vpec: peculiar motion of the Sun in the v direction in km/s
+              (Astropy Quantity), default from Schonrich 2010
+    Returns:
+        VLSR: An astropy quantity representing the velocity of the
+              local standard of rest in km/s
+    '''
+    # Compute and return v_lsr with the given formula
+    return 4.74*mu*(R_0/u.kpc)*(u.km/u.s) - vsun
 
-
-
+print("part Aa:")
+R_maser = 8.34*u.kpc # From Reid+2014
+R_GravCollab = 8.178*u.kpc # From Abuter+2019 A&A 625
+R_SG = 7.9*u.kpc# From Sparke & Gallagher
+VLSR_Reid = np.around(VLSR(R_maser), 3)
+VLSR_GravCollab = np.around(VLSR(R_GravCollab), 3)
+VLSR_SG = np.around(VLSR(R_SG), 3)
+print(f"VLSR Calcs:\nMaser: {VLSR_Reid}\nGRAVITY Collaboration: {VLSR_GravCollab}\nSparke & Gallagher: {VLSR_SG}")
 
 
 # ### b)
@@ -43,14 +65,33 @@ from astropy import constants as const # import astropy constants
 # 
 # Note that 1 km/s $\sim$ 1kpc/Gyr
 
+def getPeriod(vsun, Rsun=8.178*u.kpc ):
+    '''
+    Gets an orbital period for the Sun at a given radius with
+    a given velocity.
+    Parameters:
+        vsun: Astropy Quantity representing the velocity in km/s
+        Rsun: Orbital distance of the sun (Astropy Quantity)
+              Default from Abuter+2019
+    Returns:
+        Orbital Period of the Sun in Gyr as an Astropy Quantity
+    '''
+    vkpcGyr = vsun.to(u.kpc/u.Gyr) # convert to 
+    return (2*np.pi*Rsun) / vkpcGyr # Orbital period
 
 
-
-
-
-# ### c)
-# 
+print("part Ab:")
+vpec = 12.24*u.km/u.s
+R_GravCollab = 8.178*u.kpc # From Abuter+2019 A&A 625
+VLSR_GravCollab = np.around(VLSR(R_GravCollab), 3)
+vsun = VLSR_GravCollab + vpec
+period = getPeriod(vsun)
+print("Orbital Period of the Sun:", np.around(period, 3))
+print("Part Ac:")
 # Compute the number of rotations about the GC over the age of the universe (13.8 Gyr)
+# numRev = Age / T_sun
+numRevolutions = 13.8*u.Gyr / period
+print("Number of Revolutions in Age of Universe:", np.around(numRevolutions, 2))
 
 
 
@@ -76,6 +117,30 @@ from astropy import constants as const # import astropy constants
 # 
 # What about at 260 kpc (in units of  M$_\odot$) ? 
 
+# Mass = Integrate rho dV
+#      = rho 4*pi*r^2 dr
+#      = VLSR^2 / G/4*pi*r^2 * 4pir^2 dr
+#      = VLSR**2 / G*r
+def MassIso(r, VLSR):
+    '''
+    This function computes the mass enclosed at a given radius in the isothermal sphere
+    model.
+    Parameters:
+        r: (Astropy Quantity) Distance from the Sun to the galactic center in kpc
+        VLSR: (Astropy Quantity) Speed of local standard of rest in km/s
+    Returns:
+        M: (Astropy Quantity) Mass enclosed in within r in Solar Masses
+    '''
+    GravConst = const.G.to(u.kpc**3/(u.Gyr**2)/u.Msun) # G in desired units
+    VLSRkpcGyr = VLSR.to(u.kpc/u.Gyr)
+    M = VLSRkpcGyr**2 / GravConst * r # Mass within r
+    return M
+print("part Ba: ")
+MIsoSolar = MassIso(R_GravCollab, VLSR_GravCollab)
+print(f"Solar Radius: {MIsoSolar:.2e}")
+M260kpc = MassIso(260, VLSR_GravCollab)
+print(f"260 kpc: {M260kpc:.2e}")
+
 
 
 
@@ -95,8 +160,4 @@ from astropy import constants as const # import astropy constants
 # and assuming the Milky Way is well modeled by a Hernquist Sphere with a scale radius of $a$= 30 kpc, what is the minimum mass of the Milky Way (in units of M$_\odot$) ?  
 # 
 # How does this compare to estimates of the mass assuming the Isothermal Sphere model at 260 kpc (from your answer above)
-
-
-
-
 
